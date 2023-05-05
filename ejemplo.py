76dd1be14f03983af1301fdd18d3c17b9b8e1f2d
ALEKZLH2112 <alopez@raloy.com>
2023-05-05 10:30:04 -0600

    Este es el commit de imagen

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,9 @@
-def example():
-    print("Hola Mundo")
+def example(x,y):
+    num1 = x
+    num2 = y
+    suma = num1 + num2
+    mult = num1 * num2
+    print("Esta es mi suma {}, y esta es la multiplicacion {}".format(suma, mult))
 
-example()
+example(1,2)
 
